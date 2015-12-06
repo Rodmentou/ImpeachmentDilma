23b81901044d12cadee5884c22927840c887dc95
Rodrigo <rodmentou@gmail.com>
2015-12-06 15:21:37 -0300

    Fixed issue #3
    Workaround: timeout of 15 seconds before showing the ad.

--- www/app.js
+++ www/app.js
@@ -21,20 +21,22 @@ app.run(function($ionicPlatform) {
       StatusBar.styleDefault();
     }
 
-    if( ionic.Platform.isAndroid() )  {
-       admobid = { // for Android
-          banner: 'ca-app-pub-8150226707004967/1322407931' // Change this to your Ad Unit Id for banner...
-       };
+    setTimeout( function () {
+      if( ionic.Platform.isAndroid() )  {
+         admobid = { // for Android
+            banner: 'ca-app-pub-8150226707004967/1322407931' // Change this to your Ad Unit Id for banner...
+         };
 
-       if(AdMob)
-          AdMob.createBanner( {
-             adId:admobid.banner,
-             position:AdMob.AD_POSITION.BOTTOM_CENTER,
-             autoShow:true
-          }, function() {
-          }, function (){
-          } );
-    }
+         if(AdMob)
+            AdMob.createBanner( {
+               adId:admobid.banner,
+               position:AdMob.AD_POSITION.BOTTOM_CENTER,
+               autoShow:true
+            }, function() {
+            }, function (){
+            } );
+      }
+    }, 15000);
   });
 });
 
